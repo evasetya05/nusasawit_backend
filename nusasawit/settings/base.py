9fcfed64cbb459ce5d65f950bb7c1e5c343ef2b0
evasetya05 <you@example.com>
2025-11-26 15:58:47 +0700

    perbaikan static

--- nusasawit/settings/base.py
+++ nusasawit/settings/base.py
@@ -70,9 +70,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')

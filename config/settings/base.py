89e6241d22e9e0e949869d3a9176d241c53a3a50
evasetya05 <you@example.com>
2025-12-04 19:41:42 +0700

    prbaikan media

--- config/settings/base.py
+++ config/settings/base.py
@@ -117,8 +117,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/sdmporta/nusasawit.sdmportabel.com/media'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Auth
 AUTH_USER_MODEL = 'account.SystemUser'

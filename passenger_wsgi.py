243836d3f6368188d89256a0c00ae5560ae497d0
evasetya05 <you@example.com>
2025-12-13 13:00:45 +0700

    perbaikan passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,17 @@
+import importlib.machinery
+import importlib.util
 import os
 import sys
 
-# Tambahkan path proyek ke sys.path agar Django dapat menemukan modul pengaturan.
-# __file__ akan merujuk ke file ini (passenger_wsgi.py),
-# dan os.path.dirname(__file__) akan menjadi direktori root proyek Anda.
+
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Atur variabel lingkungan DJANGO_SETTINGS_MODULE untuk menunjuk ke file pengaturan produksi.
-# Ini memberitahu Django untuk menggunakan konfigurasi untuk lingkungan produksi.
-os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.production'
+def load_source(modname, filename):
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    loader.exec_module(module)
+    return module
 
-# Impor objek aplikasi WSGI dari file wsgi.py proyek Anda.
-# Phusion Passenger akan mencari variabel bernama 'application' secara default.
-from config.wsgi import application
+wsgi = load_source('wsgi', 'config/wsgi.py')
+application = wsgi.application

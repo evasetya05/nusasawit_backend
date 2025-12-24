3ae713977c0822fac7598c77a1dba569828d65a2
Eva Setyabudi <eva.setyabudi1986@gmail.com>
2025-12-24 20:40:52 +0700

    perbaikan deeplink7

--- api/waypoint/views.py
+++ api/waypoint/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+import logging
+
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.utils.http import urlencode
 
 
@@ -7,16 +9,39 @@ def waypoint_deep_link(request: HttpRequest) -> HttpResponse:
     """Render intermediate page to open waypoint in the NusaSawit app."""
 
     encoded_waypoint = request.GET.get("waypoint")
+    app_scheme = "nusasawit"
+    fallback_url = "https://play.google.com/store/apps/details?id=com.nusasawit.app"
+
+    if encoded_waypoint:
+        deep_link_url = f"{app_scheme}://peta?{urlencode({'waypoint': encoded_waypoint})}"
+    else:
+        deep_link_url = f"{app_scheme}://peta"
+
+    debug_mode = request.GET.get("debug") == "1"
 
     context = {
         "waypoint": encoded_waypoint,
-        "app_scheme": "nusasawit",
-        "fallback_url": "https://play.google.com/store/apps/details?id=com.nusasawit.app",
+        "app_scheme": app_scheme,
+        "app_url": deep_link_url,
+        "deep_link_url": deep_link_url,
+        "fallback_url": fallback_url,
+        "debug_mode": debug_mode,
     }
 
-    if encoded_waypoint:
-        context["app_url"] = f"nusasawit://peta?{urlencode({'waypoint': encoded_waypoint})}"
-    else:
-        context["app_url"] = "nusasawit://peta"
+    if debug_mode:
+        logger = logging.getLogger(__name__)
+        debug_info = {
+            "request_is_secure": request.is_secure(),
+            "request_scheme": request.scheme,
+            "host": request.get_host(),
+            "path": request.get_full_path(),
+            "user_agent": request.headers.get("User-Agent"),
+            "referer": request.headers.get("Referer"),
+            "deep_link_url": deep_link_url,
+            "fallback_url": fallback_url,
+            "waypoint_present": bool(encoded_waypoint),
+        }
+        context["debug_info"] = debug_info
+        logger.info("waypoint deep link debug", extra={"debug": debug_info})
 
     return render(request, "api/waypoint/deep_link.html", context)
